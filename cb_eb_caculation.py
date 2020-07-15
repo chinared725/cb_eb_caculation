ba775d166da8e70fc3c929a387ea0c08cba1c458
chinared725 <38464799+chinared725@users.noreply.github.com>
2020-07-16 01:19:23 +0800

    Update cb_eb_caculation.py

--- cb_eb_caculation.py
+++ cb_eb_caculation.py
@@ -11,8 +11,11 @@ pd.set_option('display.max_columns',100)
 bond_type = 'cb'
 bond_price = 120
 <<<<<<< HEAD
+<<<<<<< HEAD
 premium_rate = 0.15
 =======
+=======
+>>>>>>> parent of 9a4cfc7... update
 premium_rate = 0.2
 >>>>>>> parent of 9a4cfc7... update
 pb = 1
