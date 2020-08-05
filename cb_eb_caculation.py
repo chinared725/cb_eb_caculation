c4741e3a854b9d85127a1c8d6feb647cd4b6ce0d
chinared725 <38464799+chinared725@users.noreply.github.com>
2020-08-05 23:03:05 +0800

    update

--- cb_eb_caculation.py
+++ cb_eb_caculation.py
@@ -11,16 +11,17 @@ pd.set_option('display.max_columns',100)
 bond_type = 'cb'
 bond_price = 120
 
-premium_rate = 0.15
+premium_rate = 0.25
 
 pb = 1
 
 if __name__ == '__main__':
-    cb_eb = CbEb(daily_source='bs')    #类初始化
+    cb_eb = CbEb(daily_source='f')    #类初始化
 
     print('\n双低可转债，价格 < {}元，溢价率 < {}, 已上市，正股PB > {}----------------------\n\n'.format(bond_price, premium_rate, pb))
     cb = cb_eb.get_double_low_bond(bond_type=bond_type, bond_price=bond_price, premium_rate=premium_rate, pb=pb, adjusted = False)
-    print(cb[cb['当前规模_亿']<3.5])
+    cb = cb.sort_values(by=['理论偏离度'])
+    print(cb)
 
 
     print('-------------------------------------------------------------------------------------------------------')
@@ -33,6 +34,6 @@ if __name__ == '__main__':
 
     print('-------------------------------------------------------------------------------------------------------')
     print('\n你想了解的可转债可转债-----------------------------------------------------------------\n\n')
-    bond_names = ['长证转债','国君转债','浙商转债','华安转债','顺昌转债','盛屯转债']
+    bond_names = ['长证转债','国君转债','浙商转债','华安转债','顺昌转债','盛屯转债','本钢转债']
     bond = cb_eb.get_bond_data_by_name(bond_names)
     print(bond)
